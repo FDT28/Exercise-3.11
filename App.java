313a8977d378e31fece433c5d70e71309025b8d9
Fabio <fabiodtoguchi@gmail.com>
2023-12-05 09:44:21 -0500

    committed

--- App.java
+++ App.java
@@ -3,16 +3,22 @@ import java.time.YearMonth;
 public class App{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the Month (1-12): ");
-        int month = scanner.nextInt();
         System.out.println("Enter the year: ");
         int year = scanner.nextInt();
+        System.out.println("Enter the Month (1-12): ");
+        int month = scanner.nextInt();
+        while (!(month >= 1 && month <=12)) {
+            System.out.println("Invalid Month. Please enter month using numbers between 1 to 12");
+            System.out.println("Enter the Month (1-12): ");
+            month = scanner.nextInt();
+            if (month >= 1 && month <=12);
+            break;
+        }
         int daysInMonth = getDaysInMonth(year, month);
         YearMonth yearMonth = YearMonth.of(year, month);
         String monthName = yearMonth.getMonth().getDisplayName(java.time.format.TextStyle.FULL, java.util.Locale.getDefault());
         System.out.println("Number of days in " + monthName + " " + year + " is " + daysInMonth);
-
-    }
+        }
     private static int getDaysInMonth(int year, int month) {
     YearMonth yearMonth = YearMonth.of(year, month);
     int daysInMonth = yearMonth.lengthOfMonth();
