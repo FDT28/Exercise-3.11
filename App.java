e45348de0d64d499a83f41054085df5d61bfa777
Fabio <fabiodtoguchi@gmail.com>
2023-12-05 09:27:12 -0500

    Update

--- App.java
+++ App.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 import java.time.YearMonth;
-import java.time.Month;
 public class App{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -9,6 +8,7 @@ public class App{
         System.out.println("Enter the year: ");
         int year = scanner.nextInt();
         int daysInMonth = getDaysInMonth(year, month);
+        YearMonth yearMonth = YearMonth.of(year, month);
         String monthName = yearMonth.getMonth().getDisplayName(java.time.format.TextStyle.FULL, java.util.Locale.getDefault());
         System.out.println("Number of days in " + monthName + " " + year + "is " + daysInMonth);
 
